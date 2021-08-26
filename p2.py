8a300b6cc6369af2276c78d82cd3bf6a98d35de8
Chintan <munjanichintan@gmail.com>
2021-08-26 17:41:13 +0530

    p2.py modify

--- p2.py
+++ p2.py
@@ -5,26 +5,82 @@ spark = SparkSession.builder.appName("Deep Divers project-2").getOrCreate()
 df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("Deep-Divers-P2/hotel_bookings.csv")
 spark.sparkContext.setLogLevel("ERROR")
 
+# df.printSchema()
+# df.show()
+
 df.createOrReplaceTempView("hotel_booking")
 
 # 1. How many successful bookings were done in 2015?
-# q1 = spark.sql("SELECT COUNT(hotel) total_number_of_successful_booking_in_2015 from hotel_booking WHERE is_canceled=0 AND arrival_date_year=2015")
+# q1 = spark.sql(
+#     "SELECT COUNT(hotel) total_number_of_successful_booking_in_2015 \
+#     from hotel_booking \
+#     WHERE is_canceled=0 AND arrival_date_year=2015"
+# )
 # q1.show()
 
 # 2. How many companies booked hotel rooms country-wise?
-# q2 = spark.sql("select country, count(company) total_company from hotel_booking where company!='NULL' group by country order by total_company desc")
+# q2 = spark.sql(
+#     "select country, count(company) total_company \
+#     from hotel_booking \
+#     where company!='NULL' \
+#     group by country \
+#     order by total_company desc"
+# )
 # q2.show()
 
 # 3. Which room was most preferred by adults?
-# q3 = spark.sql("select reserved_room_type, count(reserved_room_type) total_count from hotel_booking where adults>0 group by reserved_room_type order by total_count desc limit 1")
+# q3 = spark.sql(
+#     "select reserved_room_type, count(reserved_room_type) total_count \
+#     from hotel_booking \
+#     where adults>0 \
+#     group by reserved_room_type \
+#     order by total_count desc \
+#     limit 1"
+# )
 # q3.show()
 
+# 5. In which month most of the booking are canceled?
+# q5 = spark.sql(
+#     "select arrival_date_month, count(is_canceled) total_canceled_booking \
+#     from hotel_booking \
+#     group by arrival_date_month \
+#     order by total_canceled_booking desc \
+#     limit 1"
+# )
+# q5.show()
+
 # 6. number of customers who stayed weekend nights and weekday nights(atleast one time)
-# q6 = spark.sql("select count(hotel) from hotel_booking where stays_in_weekend_nights>=2 and stays_in_week_nights>=5")
+# q6 = spark.sql(
+#     "select count(hotel) \
+#     from hotel_booking \
+#     where stays_in_weekend_nights>=2 and stays_in_week_nights>=5"
+# )
 # q6.show()
 
+# 8. which room most prefered by people?
+# q8 = spark.sql(
+#     "select reserved_room_type, count(reserved_room_type) total_count \
+#     from hotel_booking \
+#     group by reserved_room_type"
+# )
+# q8.show(50)
+
+# 9. Which country's people reuqested most spaecial request?
+# q9 = spark.sql(
+#     "select country, sum(total_of_special_requests) total_special_requests \
+#     from hotel_booking \
+#     group by country \
+#     order by total_special_requests desc \
+#     limit 1"
+# )
+# q9.show()
+
 # 10. list out all market segment and how many bookings lies in each market segment.
-# q10 = spark.sql("select market_segment, count(hotel) number_of_booking_lies_in_segment from hotel_booking group by market_segment")
+# q10 = spark.sql(
+#     "select market_segment, count(hotel) number_of_booking_lies_in_segment \
+#     from hotel_booking \
+#     group by market_segment"
+# )
 # q10.show()
 
 # 11. How many children+baby stayed in hotel in USA?
@@ -109,36 +165,54 @@ df.createOrReplaceTempView("hotel_booking")
 # q18.show()
 
 # 19. How many adults booked room in 2017.
-# q19 = spark.sql("select sum(adults) as No_of_Adults \
-#       from hotel_booking \
-#       where arrival_date_year=2017")
+# q19 = spark.sql(
+#     "select arrival_date_year, sum(adults) as No_of_Adults \
+#     from hotel_booking \
+#     where arrival_date_year=2017 \
+#     group by arrival_date_year"
+# )
 # q19.show()
 
 # 20. How many children were in hotel on 22-August-2015.
-# q23 = spark.sql("select count(children) \
-#       from hotel_booking \
-#       where arrival_date_year=2015 and arrival_date_month='August' and arrival_date_day_of_month=22) 
+# q20 = spark.sql(
+#     "select concat(arrival_date_day_of_month, '-', arrival_date_month, '-', arrival_date_year) as date, count(children) \
+#     from hotel_booking \
+#     where arrival_date_year=2015 and arrival_date_month='August' and arrival_date_day_of_month=22 \
+#     group by arrival_date_year, arrival_date_month, arrival_date_day_of_month"
+# ) 
 # q20.show()
 
 # 21.How many company arrived in 2015 from which country
-# q21 = spark.sql("select count(company) AS Company_Name,country \
-#       from Hotel_booking where arrival_date_year=2015 ")
+# q21 = spark.sql(
+#     "select country, count(company) as Company_Name \
+#     from hotel_booking \
+#     where arrival_date_year=2015 \
+#     group by company"
+# )
 # q21.show()
 
 # 22.  Summary of reservation status
-# q22 = spark.sql("select reservation_status \
-#       from hotel_booking") 
+# q22 = spark.sql(
+#     "select reservation_status \
+#     from hotel_booking"
+# ) 
 # q22.show()
 
 # 23. How many times does the company have a lead time between 100 and 1000
-# q23 = spark.sql("select count(company) \
-#       from hotel_booking \
-#       where lead_time between 100 and 1000")
+# q23 = spark.sql(
+#     "select company, count(company) \
+#     from hotel_booking \
+#     where lead_time between 100 and 1000 \
+#     group by company")
 # q23.show()
 
 # 24. In which month of 2016 reserved room type 'A' is most booked.
-# q24 = spar.sql("select month \
-#       from hotel_booking \
-#       where arrival_date_year=2016 and reserved_room_type='A' \
-#       order by reserved_room_type ASC")
+# q24 = spark.sql(
+#     "select arrival_date_month, count(reserved_room_type) total_room\
+#     from hotel_booking \
+#     where arrival_date_year=2015 and reserved_room_type='A' \
+#     group by arrival_date_month \
+#     order by total_room desc \
+#     limit 1"
+# )
 # q24.show()
